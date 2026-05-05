e9608d76cce2866e166528d6f4fe478e4fa572b0
tata <104385507+nftsz@users.noreply.github.com>
2026-05-05 11:19:50 -0300

    Delete core directory

--- core/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Garcom, Produto, Comanda, ItemComanda
-
-admin.site.register(Garcom)
-admin.site.register(Produto)
-admin.site.register(Comanda)
-admin.site.register(ItemComanda)
